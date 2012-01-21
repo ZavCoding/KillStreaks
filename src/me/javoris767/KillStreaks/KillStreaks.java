e4dbb82e9cd21a6982bba9867893a90229483ed2
Zach Sugano <zachoooo@gmail.com>
2012-01-21 01:17:08 -0800

    Barracks.yml :3

--- src/me/javoris767/KillStreaks/KillStreaks.java
+++ src/me/javoris767/KillStreaks/KillStreaks.java
@@ -1,7 +1,11 @@
 package me.javoris767.KillStreaks;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginManager;
@@ -10,20 +14,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class KillStreaks extends JavaPlugin {
 	public static KillStreaks plugin;
 	Logger log = Logger.getLogger("Minecraft");
-	Config config;
+	File conf;
+	FileConfiguration config;
 	@Override
 	public void onEnable() {
+		if (conf == null) {
+			conf = new File(getDataFolder(), "barracks.yml");
+		}
+		config = YamlConfiguration.loadConfiguration(conf);
+		saveConf();
 		log.info("[KillStreaks] Enabled!");
 		getCommand("ks").setExecutor(new KillStreaksCMDs(this));
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_INTERACT_ENTITY, new KSPlayerListener(this), Priority.Normal, this);
 		//pm.registerEvent(Event.Type.BLOCK_DAMAGE, new PrecisionAirstrikeListener(this), Priority.Highest, this);
 		//pm.registerEvent(Event.Type.CREATURE_SPAWN, new AttackDogListener(this), Priority.Highest, this);
-		config = new Config(this);
-		config.loadConfig();
 	}
 	@Override
 	public void onDisable() {
 		log.info("[KillStreaks] Disabled!");
+	}
+	public void saveConf() {
+		if (conf == null || config == null) {
+		}
+		try {
+			config.save(conf);
+		} catch (IOException e) {
+			log.severe("[KillStreaks] Unable to Save Config.");
+		}
 	}
 }
