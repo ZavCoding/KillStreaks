4d7f3baa3a831e216326586a87eaefa99b63c39d
Cody Jackson <sasukeucheha@live.com>
2012-01-21 02:32:14 -0800

    commit

--- src/me/javoris767/KillStreaks/Config.java
+++ src/me/javoris767/KillStreaks/Config.java
@@ -1,56 +1,18 @@
 package me.javoris767.KillStreaks;
 
-import java.io.File;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.Configuration;
 
 public class Config {
-	public KillStreaks plugin;
+private final KillStreaks plugin;
     public Config(KillStreaks plugin) {
         this.plugin = plugin;
     }
-    private static File KillStreaks = new File("plugins", "ConfigTest");
-    public static String playername;
-    private static File configFile = new File(KillStreaks, playername + ".yml");
-    public static FileConfiguration config;
+    public Configuration config;
 
-    public static void firstRun() {
-        config = new YamlConfiguration();
-
-        if (!KillStreaks.exists()) {
-            try {
-                KillStreaks.mkdir();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (!configFile.exists()) {
-            try {
-                configFile.createNewFile();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            config.set("Path.to.message", "Config message");
-            try {
-                config.save(configFile);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        loadConfig();
-    }
-
-    private static void loadConfig() {
-        config = new YamlConfiguration();
-        try {
-            config.load(configFile);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String msg = config.getString("Path.to.message");
-        System.out.println(msg);
+    public void loadConfig() {
+        config = plugin.getConfig();
+        config.addDefault("", "");
+        config.options().copyDefaults(true);
+        plugin.saveConfig();
     }
 }
--- src/me/javoris767/KillStreaks/KillStreaks.java
+++ src/me/javoris767/KillStreaks/KillStreaks.java
@@ -1,17 +1,16 @@
 package me.javoris767.KillStreaks;
 
-import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
 public class KillStreaks extends JavaPlugin {
 	public static KillStreaks plugin;
 	Logger log = Logger.getLogger("Minecraft");
+	Config config;
 	@Override
 	public void onEnable() {
 		log.info("[KillStreaks] Enabled!");
@@ -20,6 +19,8 @@ public class KillStreaks extends JavaPlugin {
 		pm.registerEvent(Event.Type.PLAYER_INTERACT_ENTITY, new KSPlayerListener(this), Priority.Normal, this);
 		//pm.registerEvent(Event.Type.BLOCK_DAMAGE, new PrecisionAirstrikeListener(this), Priority.Highest, this);
 		//pm.registerEvent(Event.Type.CREATURE_SPAWN, new AttackDogListener(this), Priority.Highest, this);
+		config = new Config(this);
+		config.loadConfig();
 	}
 	@Override
 	public void onDisable() {
