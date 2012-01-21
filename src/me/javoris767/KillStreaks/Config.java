1037b78b65b9f5a95da98425beab49bde3ca1beb
Cody Jackson <sasukeucheha@live.com>
2012-01-21 02:22:09 -0800

    Pushing

--- src/me/javoris767/KillStreaks/Config.java
+++ src/me/javoris767/KillStreaks/Config.java
@@ -1,19 +1,56 @@
 package me.javoris767.KillStreaks;
 
-import org.bukkit.configuration.Configuration;
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Config {
-	private final KillStreaks plugin;
+	public KillStreaks plugin;
     public Config(KillStreaks plugin) {
         this.plugin = plugin;
     }
-    public Configuration config;
+    private static File KillStreaks = new File("plugins", "ConfigTest");
+    public static String playername;
+    private static File configFile = new File(KillStreaks, playername + ".yml");
+    public static FileConfiguration config;
 
-    public void loadConfig() {
-        config = plugin.getConfig();
-        config.addDefault("", "");
-        config.options().copyDefaults(true);
-        plugin.saveConfig();
+    public static void firstRun() {
+        config = new YamlConfiguration();
+
+        if (!KillStreaks.exists()) {
+            try {
+                KillStreaks.mkdir();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            config.set("Path.to.message", "Config message");
+            try {
+                config.save(configFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        loadConfig();
+    }
+
+    private static void loadConfig() {
+        config = new YamlConfiguration();
+        try {
+            config.load(configFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String msg = config.getString("Path.to.message");
+        System.out.println(msg);
     }
 }
-
